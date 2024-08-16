468fd3b38eb4eb1bf34ed431a935c601cfef127b
mxie11 <mingxin0129e@gmail.com>
2024-08-16 16:07:56 -0700

    Initial Commit

---
+++ FetchChallenge/FetchChallengeApp.swift
@@ -0,0 +1,26 @@
+//
+//  FetchChallengeApp.swift
+//  FetchChallenge
+//
+//  Created by Mingxin Xie on 8/16/24.
+//
+
+import SwiftUI
+
+/*
+    This is a single and simple app that is built on MVC pattern.
+ 
+    The users should be shown the list of meals of one category, sorted alphabetically.
+ 
+    When users tap on a meal, it will display a details view of the meal.
+
+ */
+
+@main
+struct FetchChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FetchChallenge/Model/DishModel.swift
@@ -0,0 +1,13 @@
+//
+//  DishModel.swift
+//  FetchChallenge
+//
+//  Created by Mingxin Xie on 8/16/24.
+//
+
+import Foundation
+
+struct Dish: Codable, Identifiable {
+    var id: String
+    
+}
