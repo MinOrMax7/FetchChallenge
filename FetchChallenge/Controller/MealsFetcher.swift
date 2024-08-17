fc9fcb63ad2cabee0c66d9c2d1185175db0be132
mxie11 <mingxin0129e@gmail.com>
2024-08-16 17:24:02 -0700

    add first design of data model for meals, untested

---
+++ FetchChallenge/Controller/MealsFetcher.swift
@@ -0,0 +1,19 @@
+//
+//  DishFetcher.swift
+//  FetchChallenge
+//
+//  Created by Mingxin Xie on 8/16/24.
+//
+
+import Foundation
+
+/*
+    async function to do data fetching by decoding a json file
+    through GET api call. Return to the fetching results.
+ **/
+
+struct DishFetcher {
+    // Fetches and returns a list of `Place`s with their `imageURL` property set.
+    static func fetchDish
+    
+}
--- FetchChallenge/Model/DishModel.swift
+++ FetchChallenge/Model/DishModel.swift
@@ -7,7 +7,40 @@
 
 import Foundation
 
-struct Dish: Codable, Identifiable {
+/// Codable for Json decoder
+/// Identifiable to list view
+
+/// Data Model representing a meal
+struct Meal: Codable, Identifiable {
     var id: String
+    var name: String
+    var imageURL: URL?
     
+    enum CoddingKeys: String, CodingKey {
+        case id = "idMeal"
+        case name = "strMeal"
+        case imageURL = "strMealThumb"
+    }
 }
+extension Meal {
+    static var exampleMeal: Meal = Meal(id: "00000", name: "Sample Dish")
+}
+
+/// Data Model representing a dessert
+struct Dessert: Codable, Identifiable {
+    var id: String
+    var name: String
+    var imageURL: URL?
+    var instructions: String
+    var reciepts: [String: String]
+    
+    var ingredients: [String]
+    var measurements: [String]
+}
+
+
+/// Response from Dish API
+struct MealsResult: Codable {
+    var meals: [Meal]
+}
+
